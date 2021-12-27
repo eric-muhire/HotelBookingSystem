8cd85156d478e2167153631ac3346cacfecf6b3f
eric-at-467699451292 <eric@gmail.com>
2021-12-27 10:47:01 +0100

    I have created class customer, customerController and CustomerService

--- src/main/java/com/hotelBookingSystem/group/HotelBookingSystemWebAppApplication.java
+++ src/main/java/com/hotelBookingSystem/group/HotelBookingSystemWebAppApplication.java
@@ -1,14 +1,19 @@
 package com.hotelBookingSystem.group;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @SpringBootApplication
+@RestController
 public class HotelBookingSystemWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingSystemWebAppApplication.class, args);
 	}
-//hej
-	//hej Eric and Sara
+
+
+
 }
+
