95fb001217128c03216af2275d9fc4f093719a6b
eric-at-467699451292 <eric@gmail.com>
2022-01-03 20:41:52 +0100

    I have created DynamoDBConfig file. Updated constructor with lombok in customer class.Update customerController with getMapping, deleteMapping and putMapping

--- src/main/java/com/hotelBookingSystem/group/controllers/CustomerController.java
+++ src/main/java/com/hotelBookingSystem/group/controllers/CustomerController.java
@@ -1,28 +1,41 @@
 package com.hotelBookingSystem.group.controllers;
 import com.hotelBookingSystem.group.models.Customer;
-import com.hotelBookingSystem.group.services.CustomerService;
+import com.hotelBookingSystem.group.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/customer")
+//@RequestMapping(path = "api/v1/customer")
 public class CustomerController {
 
-    private final CustomerService CustomerService;
-
+    //private final CustomerService CustomerService;
     @Autowired
-    public CustomerController(CustomerService customerService) {
-        this.CustomerService = customerService;
+    private CustomerRepository customerRepository;
+
+    @PostMapping("/customer")
+    public Customer saveCustomer(@RequestBody Customer customer){
+        return customerRepository.save(customer);
     }
 
+   // @Autowired
+    //public CustomerController(CustomerService customerService) {
+      //  this.CustomerService = customerService;
+   // }
 
-    @GetMapping
-    public List<Customer> getCustomer() {
-        return CustomerService.getCustomer();
 
+    @GetMapping("/customer/{id}")
+    public Customer getCustomer(@PathVariable("id") String customerId) {
+        return customerRepository.getCustomerById(customerId);
+
+    }
+    @DeleteMapping("/customer/{id}")
+    public String deleteCustomer(@PathVariable("id") String customerId){
+        return customerRepository.delete(customerId);
+    }
+
+    @PutMapping("/customer({id}")
+    public String updateCustomer(@PathVariable("id") String customerId, @RequestBody Customer customer){
+        return customerRepository.update(customerId,customer);
     }
 }
--- src/main/java/com/hotelBookingSystem/group/services/CustomerService.java
+++ src/main/java/com/hotelBookingSystem/group/services/CustomerService.java
@@ -1,22 +1,22 @@
-package com.hotelBookingSystem.group.services;
-import com.hotelBookingSystem.group.models.Customer;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class CustomerService {
-    public List<Customer> getCustomer() {
-        return List.of(new Customer(
-                100L,
-                "Anna",
-                "Andersson",
-                "+4755555",
-                "e.hotmail.com",
-                "västergatan 10",
-                "Gothenburg",
-                "45595",
-                "Sweden"));
-
-    }
-}
+//package com.hotelBookingSystem.group.services;
+//import com.hotelBookingSystem.group.models.Customer;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class CustomerService {
+//    public List<Customer> getCustomer() {
+//        return List.of(new Customer(
+//                100L,
+//                "Anna",
+//                "Andersson",
+//                "+4755555",
+//                "e.hotmail.com",
+//                "västergatan 10",
+//                "Gothenburg",
+//                "45595",
+//                "Sweden"));
+//
+//    }
+//}
