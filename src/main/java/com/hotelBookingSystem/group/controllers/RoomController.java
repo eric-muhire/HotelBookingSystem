bea4d3bcc73f6e7b4de5d2f0d86764d8026e7c4e
saralofwander <saralofwander@gmail.com>
2022-01-06 15:34:29 +0100

    Remove unused api entry point

--- src/main/java/com/hotelBookingSystem/group/controllers/RoomController.java
+++ src/main/java/com/hotelBookingSystem/group/controllers/RoomController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/room")
 public class RoomController {
 
     private final RoomService roomService;
