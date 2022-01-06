1d3094c5971fc124482ea86b3ee6ccd7eb14fe20
eric-at-467699451292 <eric@gmail.com>
2022-01-06 12:37:17 +0100

    .feat: Added CRUD operation to featuresController.

--- src/main/java/com/hotelBookingSystem/group/services/FeaturesService.java
+++ src/main/java/com/hotelBookingSystem/group/services/FeaturesService.java
@@ -1,16 +1,16 @@
-package com.hotelBookingSystem.group.services;
-
-import com.hotelBookingSystem.group.models.Features;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class FeaturesService {
-
-    public List<Features> getFeatures(){
-        return List.of(new Features(
-
-        ));
-    }
-}
+//package com.hotelBookingSystem.group.services;
+//
+//import com.hotelBookingSystem.group.models.Features;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class FeaturesService {
+//
+//    public List<Features> getFeatures(){
+//        return List.of(new Features(
+//
+//        ));
+//    }
+//}
